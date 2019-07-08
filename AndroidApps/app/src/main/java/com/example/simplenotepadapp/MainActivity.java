39788c3a2bc10a7c58cb25860fc6fbf686765985
Patrick <paddymcpherson@hotmail.com>
2019-07-08 03:26:05 +0100

    yo yo
    	new file:   AndroidApps/.gitignore
    	new file:   AndroidApps/.idea/encodings.xml
    	new file:   AndroidApps/.idea/gradle.xml
    	new file:   AndroidApps/.idea/misc.xml
    	new file:   AndroidApps/.idea/runConfigurations.xml
    	new file:   AndroidApps/app/.gitignore
    	new file:   AndroidApps/app/build.gradle
    	new file:   AndroidApps/app/proguard-rules.pro
    	new file:   AndroidApps/app/src/androidTest/java/com/example/simplenotepadapp/ExampleInstrumentedTest.java
    	new file:   AndroidApps/app/src/main/AndroidManifest.xml
    	new file:   AndroidApps/app/src/main/java/com/example/simplenotepadapp/MainActivity.java
    	new file:   AndroidApps/app/src/main/res/drawable-v24/ic_launcher_foreground.xml
    	new file:   AndroidApps/app/src/main/res/drawable/ic_launcher_background.xml
    	new file:   AndroidApps/app/src/main/res/layout/activity_main.xml
    	new file:   AndroidApps/app/src/main/res/layout/content_main.xml
    	new file:   AndroidApps/app/src/main/res/menu/menu_main.xml
    	new file:   AndroidApps/app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml
    	new file:   AndroidApps/app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml
    	new file:   AndroidApps/app/src/main/res/mipmap-hdpi/ic_launcher.png
    	new file:   AndroidApps/app/src/main/res/mipmap-hdpi/ic_launcher_round.png
    	new file:   AndroidApps/app/src/main/res/mipmap-mdpi/ic_launcher.png
    	new file:   AndroidApps/app/src/main/res/mipmap-mdpi/ic_launcher_round.png
    	new file:   AndroidApps/app/src/main/res/mipmap-xhdpi/ic_launcher.png
    	new file:   AndroidApps/app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
    	new file:   AndroidApps/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    	new file:   AndroidApps/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
    	new file:   AndroidApps/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    	new file:   AndroidApps/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
    	new file:   AndroidApps/app/src/main/res/values/colors.xml
    	new file:   AndroidApps/app/src/main/res/values/dimens.xml
    	new file:   AndroidApps/app/src/main/res/values/strings.xml
    	new file:   AndroidApps/app/src/main/res/values/styles.xml
    	new file:   AndroidApps/app/src/test/java/com/example/simplenotepadapp/ExampleUnitTest.java
    	new file:   AndroidApps/build.gradle
    	new file:   AndroidApps/gradle.properties
    	new file:   AndroidApps/gradle/wrapper/gradle-wrapper.jar
    	new file:   AndroidApps/gradle/wrapper/gradle-wrapper.properties
    	new file:   AndroidApps/gradlew
    	new file:   AndroidApps/gradlew.bat
    	new file:   AndroidApps/settings.gradle

---
+++ AndroidApps/app/src/main/java/com/example/simplenotepadapp/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.simplenotepadapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void Save(String fileName) {
+        try {
+            OutputStreamWriter out =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(EditText1.getText().toString());
+            out.close();
+            Toast.makeText(this, "Note Saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    EditText EditText1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        EditText1 = findViewById(R.id.EditText1);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Save("Note1.txt");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
